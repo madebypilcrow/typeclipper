675ae51ef6af550fbd0f200c7918e73e6ae17b86
Dan Beltechi <42743129+madebypilcrow@users.noreply.github.com>
2025-11-03 18:23:44 +0200

    fix vite base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // no subpath since you'll host at root or custom domain later
+  base: '/typeclipper/', // repo name
 });
