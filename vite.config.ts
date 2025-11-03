4f77e14262d9ba9740c329c27b8423b38a47bbf0
Dan Beltechi <42743129+madebypilcrow@users.noreply.github.com>
2025-11-03 18:11:26 +0200

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // no subpath since you'll host at root or custom domain later
+});
